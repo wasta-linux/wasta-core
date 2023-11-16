5b1a1e05b210e0b43af7f1a6ca3982eae6190519
Justin Luth <jluth@mail.com>
2023-11-16 10:50:37 -0500

    tried to pre-select lightdm again, but still fails
    Documented the attempt by adding the --unseen flag
    which should tell me next time that it really doesn't work.

--- wasta-core/wasta-core-postinst.sh
+++ wasta-core/wasta-core-postinst.sh
@@ -208,7 +208,7 @@ fi
 # ------------------------------------------------------------------------------
 # lightdm: set as display manager:
 #echo "lightdm shared/default-x-display-manager select lightdm" \
-#    | debconf-set-selections
+#    | debconf-set-selections --unseen
 
 # libdvd-pkg
 # don't think needed: libdvd-pkg libdvd-pkg/post-invoke_hook-remove boolean false
